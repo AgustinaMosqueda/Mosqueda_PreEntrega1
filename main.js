093b44814224c0d8fc59aac9885fc340eb029006
agustina mosqueda <brisaagustina_@outlook.com>
2023-10-02 18:21:59 -0300

    unico cambio

--- main.js
+++ main.js
@@ -1,3 +1,49 @@
-let nombre = "Agustina";
-let apellido = "Mosqueda";
-let edad = "23";
+let dias = prompt("quiere saber cuantos días trae el mes? si/no");
+
+if (dias == "si" ) {
+    alert= prompt("elije el mes deseado");
+    }
+    else{
+    alert("que tengas bonito día");
+}
+switch ("elije el mes deseado") { 
+    case enero:
+        console.log(31);
+        break;
+    case febrero:
+        console.log(31);
+        break;
+    case marzo:
+        console.log(31);
+        break;
+    case abril:
+        console.log(31);
+        break;
+    case mayo:
+        console.log(31);
+        break;
+    case junio:
+        console.log(31);
+        break;
+    case julio:
+        console.log(31);
+        break;
+    case agosto:
+        console.log(31);
+        break;
+    case septiembre:
+        console.log(31);
+        break;
+    case octubre:
+        console.log(31);
+        break;
+    case noviembre:
+        console.log(31);
+        break;
+    case diciembre:
+        console.log(31);
+        break;
+        default:
+            console.log ("ese mes no existe");
+            break;
+};
